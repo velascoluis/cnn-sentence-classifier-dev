97c523660dd4f31328c37df0d8b6feb42a41e441
velascoluis <velascoluis@google.com>
2020-04-08 14:04:52 +0200

    Add README.md for root

--- observability/01_drift_validator/src/drift_validator.py
+++ observability/01_drift_validator/src/drift_validator.py
@@ -1,6 +1,7 @@
 #@to-commit
 import tensorflow_data_validation as tfdv
 print('TFDV version: {}'.format(tfdv.version.__version__))
-
+train_stats = tfdv.generate_statistics_from_csv(data_location='/Users/velascoluis/PycharmProjects/cnn-sentence-classifier-dev/local/data/airplaneData.csv')
+tfdv.visualize_statistics(train_stats)
 
 
