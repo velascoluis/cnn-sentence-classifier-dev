3f20bfccf9103e3ac08a274b1dae2e0b7db8c5f9
velascoluis <velascoluis@google.com>
2020-04-08 12:58:51 +0200

    Add README.md for root

--- observability/01_drift_validator/src/drift_validator.py
+++ observability/01_drift_validator/src/drift_validator.py
@@ -1,9 +1,4 @@
-import argparse
-from datetime import datetime
-import logging
-import json
-from kubeflow.metadata import metadata
-import retrying
+#@to-commit
 import tensorflow_data_validation as tfdv
 print('TFDV version: {}'.format(tfdv.version.__version__))
 
